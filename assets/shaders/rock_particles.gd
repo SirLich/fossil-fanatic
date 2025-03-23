4b140872acc1c59da06dd4438ec137490e1a1b0c
SirLich <lrkoehler77@gmail.com>
2025-03-23 17:35:55 +0100

    Try version without stutter

--- assets/shaders/rock_particles.gd
+++ assets/shaders/rock_particles.gd
@@ -3,15 +3,7 @@ extends Node2D
 @export var particles : Array[GPUParticles2D]
 
 var finished_particles = 0
-
-func _evaluate_despawn_conditions():
-	finished_particles += 1
-	if finished_particles == particles.size():
-		queue_free()
-
 	
 func _ready() -> void:
-	await get_tree().process_frame
-	
-	for particle in particles:
-		particle.finished.connect(_evaluate_despawn_conditions)
+	await get_tree().create_timer(2).timeout
+	queue_free()
